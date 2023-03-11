5822b97850ec80b74005c3356aa2474b2dadf037
Calvin Nguyen <calvinnguyen50@gmail.com>
2023-03-11 12:39:18 -0800

    imported components from containers and components into App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { Footer, Blog, Possibility, Features,
+WhatGPT3, Header } from './containers'
+import { CTA, Brand, NavBar } from './components'
+
 const App = () => {
   return (
     <div>
