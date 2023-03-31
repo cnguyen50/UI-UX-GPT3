e1da57efe69bcc93f1b3b8fb8376bf128e04ac24
Calvin Nguyen <calvinnguyen50@gmail.com>
2023-03-30 20:17:08 -0700

    toggle menu state

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -19,7 +19,8 @@ const Navbar = () => {
           <p><a href="#features">Case Studies</a></p>
           <p><a href="#blog">Library</a></p>
         </div>
-        <div className="gpt3__navbar-sign">
+      </div>
+      <div className="gpt3__navbar-sign">
         <p>Sign in</p>
         <button type="button">Sign up</button>
       </div>
@@ -28,11 +29,24 @@ const Navbar = () => {
           ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
           : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
         }
-        
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <p><a href="#home">Home</a></p>
+              <p><a href="#wgpt3">What is GPT3?</a></p>
+              <p><a href="#possibility">Open AI</a></p>
+              <p><a href="#features">Case Studies</a></p>
+              <p><a href="#blog">Library</a></p>
+            </div>
+            <div className="gpt3__navbar-menu_container-links-sign">
+              <p>Sign in</p>
+              <button type="button">Sign up</button>
+            </div>
+          </div>
+        )}
       </div>
-      </div>
-    </div>  
+    </div>
   );
-}
+};
 
 export default Navbar;
