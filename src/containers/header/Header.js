1844e8de4c2cdf5b8e5fd73bc4963f0865416d97
Calvin Nguyen <calvinnguyen50@gmail.com>
2023-06-06 18:17:55 -0700

    paragraph added

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -6,7 +6,8 @@ const Header = () => {
     <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
         <h1 className="gradient__text">Let's Build Something amazing with GPT-3 OpenAI</h1>
-
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        
         <div className="gpt3__header-content__input">
           <input type="email" placeholder="Your Email     Address" />
           <button type="button">Get Started</button>
