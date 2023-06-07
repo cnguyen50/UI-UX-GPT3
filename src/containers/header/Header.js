65c42608a88aa9c4b57286a2cb3eec190d3d1671
Calvin Nguyen <calvinnguyen50@gmail.com>
2023-06-06 18:16:56 -0700

    email input with button

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -6,6 +6,11 @@ const Header = () => {
     <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
         <h1 className="gradient__text">Let's Build Something amazing with GPT-3 OpenAI</h1>
+
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email     Address" />
+          <button type="button">Get Started</button>
+        </div>
       </div>
       header
     </div>
